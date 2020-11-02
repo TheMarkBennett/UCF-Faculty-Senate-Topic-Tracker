8295d5fef0689b96c80f3b3f58c381be069761f0
Mark <mark.bennett@ucf.edu>
2020-11-02 09:30:03 -0500

    reverse status rework

--- templates/ucf_fs_topic_tracker_topic_status.php
+++ templates/ucf_fs_topic_tracker_topic_status.php
@@ -9,13 +9,15 @@ function ucf_fs_issue_template_topic_status() {
 */
 
 $statusRepeater = get_field('topic_tracker_status_update'); 
+if($statusRepeater){
 $statusOrder = array_reverse($statusRepeater);
+}
 
 /*
 *	Status update layout with ACF repater fields using foreach
 */
 
-if( $statusOrder ): ?>
+if( isset($statusOrder) ): ?>
 <section class="fs-topic-status-updates mt-4"> 
 	<h2 class="mb-5 mt-3" >Status Updates</h2>
 	<?php foreach( $statusOrder as $i => $row ): ?>
@@ -37,36 +39,6 @@ if( $statusOrder ): ?>
 			</div>
 		</div>
 		
-		
-		
-		
-		<?php
-		/*
-		*	Reverse comments order
-		*/
-		/*
-		$commentsRepeater = $row['topic_tracker_comments_list']; 
-		$commentsOrder = array_reverse($commentsRepeater); 
-		
-		?>
-		<?php if( $commentsOrder ):?>
-			
-		<div class="pb-3 mx-3">
-			<div><a href="#" class="btn btn-outline-secondary btn-sm" style="font-size: .8em;" aria-label="View comments" data-toggle="collapse" data-target="#accordion-<?php echo $i; ?>" aria-expanded="false">View Comments</a></div>
-			<div class="m-0 mt-3  py-3 collapse" id="accordion-<?php echo $i; ?>" itemscope="" itemprop="acceptedAnswer">
-				<h3 class="h5">Comments</h3>
-				<ul class="list-unstyled ">
-				
-				<?php foreach( $commentsOrder as $i => $rows ): ?>
-					<li>
-						<p class="small"><strong><?php echo esc_html($rows['topic_tracker_commenter']); ?> (<?php echo esc_html($rows['topic_tracker_comment_date']); ?>) - </strong> <?php echo $rows['topic_tracker_comment']; ?></p>						
-					</li>
-				<?php endforeach; ?>
-			</div>
-		</div>
-		<?php endif; */ ?>
- 
-
 
 
 	</div>
